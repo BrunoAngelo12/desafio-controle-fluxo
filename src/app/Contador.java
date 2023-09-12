626e24bde08a64b9c60ae9610a4e4fc16e922b14
Brn <bruno.trabalhos@outlook.com>
2023-09-11 21:45:48 -0300

    criado a base de interação com o usuário, o método de validação juntamente com a exceção

--- src/app/Contador.java
+++ src/app/Contador.java
@@ -1,5 +1,26 @@
+package app;
+
+import java.util.Scanner;
+
+import exceptions.ParametrosInvalidosException;
+
 public class Contador {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the first parameter: ");
+        int firstParameter = sc.nextInt();
+        System.out.print("Enter the second parameter: ");
+        int secondParameter = sc.nextInt();
+        count(firstParameter, secondParameter);
+
+        sc.close();
     }
+
+    static void count (int firstParameter, int secondParameter) throws ParametrosInvalidosException{
+        System.out.println("hello");
+    }
+
+    
+
 }
